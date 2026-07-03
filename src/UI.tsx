98b429ba52ba4804de21469df0fb52d1a4842285
Oskar Niesen <oskarniesen1@gmail.com>
2026-07-03 12:18:43 -0500

    Added a floating balance modifier text

--- src/UI.tsx
+++ src/UI.tsx
@@ -7,6 +7,7 @@ type BalanceNumberProps = {
 
 function BalanceNumber({gameState, balance}: BalanceNumberProps) {
     const [previousBalance, setPreviousBalance] = useState(balance);
+    const [targetBalance, setTargetBalance] = useState(balance)
     const [increaseAnimationState, setIncreaseAnimationState] = useState(balance);
     const [displayedBalance, setDisplayedBalance] = useState(balance)
     useEffect(() => {
@@ -19,16 +20,22 @@ function BalanceNumber({gameState, balance}: BalanceNumberProps) {
                 } else {
                     setIncreaseAnimationState(() => increaseAnimationState + 1);
                 }
-            } else if (balance != previousBalance) {
+            }
+            if (balance != targetBalance) {
                 setIncreaseAnimationState(0);
+                setTargetBalance(balance);
             }
 
         }, 100);
         return () => clearTimeout(interval);
     }, [balance, previousBalance, increaseAnimationState]);
     return (
-        <h2 className="text-gray-700! justify-self-end mt-2">{gameState.formatter.format(displayedBalance)}</h2>
-    );
+        <h2 className="text-gray-700! justify-self-end mt-2 h-9" id="balancetext">
+            {gameState.formatter.format(displayedBalance)}
+            {increaseAnimationState >= 0 ?
+                <h2 className="relative text-green-400! left-0 bottom-22"
+                >{balance - previousBalance > 0 ? "+" : ""}
+                    {gameState.formatter.format(balance - previousBalance)}</h2> : <></>}</h2>);
 }
 
 export default BalanceNumber;
